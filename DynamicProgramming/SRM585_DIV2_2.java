ea6764fd63c68c0533f6da6dd1750c9a7514a842
Manohar Prabhu <manoharprabhu@manoharprabhu.(none)>
2013-12-16 17:36:00 +0530

    Made SRM 585 DIV 2 solution more efficient.It passes DIV 1 test cases now.

--- DynamicProgramming/SRM585_DIV2_2.java
+++ DynamicProgramming/SRM585_DIV2_2.java
@@ -1,20 +1,13 @@
-public class TrafficCongestionDivTwo {
-	long[] table = new long[100];
+public class TrafficCongestion {
+	long[] table = new long[1000001];
 	public long theMinCars(int treeHeight) {
-if(table[treeHeight] > 0)
-	return table[treeHeight];
-		if(treeHeight == 0 || treeHeight == 1)
-			return 1;
-		else {
-			long sum= 0;
-			
-			for(int i=0;i<=treeHeight - 2;i++){
-				sum = sum + theMinCars(i);
-				
-			}
-			table[treeHeight] = 1 + 2*(sum);
-			return table[treeHeight];
+		table[0] = 1;
+		table[1] = 1;
+		for(int i=2;i<=treeHeight;i++){
+			table[i] = (2*table[i-2]+table[i-1])% 1000000007;
 		}
+		
+		return table[treeHeight];
+		
 	}
-
-}
+};
